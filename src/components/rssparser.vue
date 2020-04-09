a22afda89e1a2cbb204684ff6c349118fc8cd5d8
Fabian Karl <elvi.karl@web.de>
2020-04-10 01:03:43 +0200

    rssparser lib added

---
+++ src/components/rssparser.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Navigation/>
+    <VueRssFeed
+      :feedUrl="feedUrl"
+      :name="name"
+      :limit="limit"
+    />
+  </div>
+</template>
+
+<script>
+  import VueRssFeed from 'vue-rss-feed';
+
+  export default {
+    name: "rssparser",
+    components:
+      {
+        VueRssFeed
+      },
+    props:['url'],
+    data() {
+      return {
+        feedUrl: this.url,
+        name: "",
+        limit: 5,
+      };
+    },
+  };
+
+</script>
