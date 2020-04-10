f126bf62cd6ca197215e4fdc3449b8cecbb119b5
sebastianjulis <seb.weiss2003@yahoo.de>
2020-04-10 14:53:50 +0200

    lak digitales hinzugefügt

--- src/views/LAK_Digitales.vue
+++ src/views/LAK_Digitales.vue
@@ -1,10 +1,57 @@
 <template>
   <div>
-
     <div id="lakbackground">
-      <i class="las la-sitemap"></i>
+      <i class="las la-sitemap la-8x"></i>
       <h1>LAK DIGITALES & MEDIEN</h1>
+    </div>
+    <div class="container">
+      <p>IT ist ein essentieller Bestandteil unseres Lebensalltags.
+      Sie kann individuelle Freiheit beflügeln und Innovation fördern, aber auch Menschen unterdrücken und wenigen Akteuren überproportional viel Macht über unsere gesellschaft verleihen.</p>
+      Dieser LAK befasst sich mit der Frage was Politik tun oder lassen sollte,
+      um IT zum Wohle der Gesellschaft und nicht zu ihrem Schaden zu gebrauchen.<br>
+      <div style="text-align:center;">
+        <button type="button" class="button-cyan">JETZT ZUM LAK ANMELDEN</button>
+      </div>
 
+       <div class="card mb-3 border-custom mx-auto" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://via.placeholder.com/50" class="card-img" alt="Timo Breuninger">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">LEITUNG</h5>
+              <h5 class="card-title2">TIMO BREUNINGER</h5>
+              <p class="card-text">Elektroingenieur</p>
+              <div id="card-link">
+               <a href="mailto:max.m@example.com"><i class="las la-envelope la-sm"></i> NACHRICHT SCHREIBEN</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card mb-3 border-custom mx-auto" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://via.placeholder.com/50" class="card-img" alt="Mirjam Aron">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">STELLVERTRETERIN</h5>
+              <h5 class="card-title2">MIRJAM ARON</h5>
+              <p class="card-text">Studentin der Rechtswissenschaft</p>
+              <div id="card-link">
+               <a href="mailto:max.m@example.com"><i class="las la-envelope la-sm"></i> NACHRICHT SCHREIBEN</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>Du interessierst dich für das Thema Digitales & Meiden und möchtestdich im Landesarbeitskreis engagieren?</p>
+      <p>Dann trage dich jetzt in den Verteiler ein, um über alle Termine und Neuigkeiten informiert zu sein!</p>
+      <div style="text-align:center;">
+        <button type="button" class="button-cyan">JETZT ZUM LAK ANMELDEN</button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,11 +64,65 @@
 
 <style scoped>
 
+.container{
+  font-weight: bold; padding: 5% 8% -5% 8%;
+}
 h1, .la-sitemap{
-  text-align: center; color: white;
+  text-align: center;
+  color: white;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 #lakbackground{
-  width: 100%; background-color: $magenta;
+  width: 100%; background-color: #e5007d;
 }
-
+.la-sitemap{
+  padding-bottom: 1%;
+  padding-top: 5%;
+}
+h1{
+  font-size: 150%;
+  font-weight: bolder;
+  padding-bottom: 7%;
+}
+.button-cyan{
+  background-color: #009EE3;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+  margin:7% auto;
+  cursor: pointer;
+  border-radius: 8px;
+}
+.button-cyan:hover{
+  background-color: #008b8b;
+}
+.card-title{
+  font-weight: bold;
+  color: black;
+}
+.card-title2{
+  font-weight:bolder; 
+  color: #e5007d;
+}
+.card-text{
+ font-weight:bold; 
+ color: #e5007d;
+ margin-top: -3%;
+}
+#card-link a:link{
+  color: black;
+}
+#card-link a:visited{
+  color: black;
+}
+.border-custom{
+  border-color: #e5007d;
+  border-width: medium;
+}
 </style>
