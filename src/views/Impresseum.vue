118f35c7f2fba8e04c7d5129aa1e117a125719bb
marvw <marvin.wank@outlook.de>
2020-04-10 02:05:13 +0100

    Verlagere Style-Klassen ins globale

--- src/views/Impresseum.vue
+++ src/views/Impresseum.vue
@@ -1,65 +1,81 @@
 <template>
-  <div>
+  <div class="container">
+    <h1 class="text-magenta text-center mt-3">ABOUT</h1>
 
-<html>
-<head>
-    <title>About</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-</head>
-<body>
-    <div class="container">
-        <h1>ABOUT</h1>
-        
-        <nav class="nav nav-tabs nav-fill">
-          <a class="nav-item nav-link active" href="about.html#datenschutz">Datenschutz</a>
-          <a class="nav-item nav-link" href="#">Technologien</a>
-          <a class="nav-item nav-link" href="#">Entwickler</a>
-        </nav>
-        
-        <div id="datenschutz">
-            <h2>DATENSCHUTZERKLÄRUNG</h2>
-            <hr>
-            <h3>INFORMATION ÜBER DIE ERHEBUNG PERSONENBEZOGENER DATEN</h3>
-            Im Folgenden informieren wir über Art, Umfang und Zwecke der Erhebung und Verarbeitung personenbezogener Daten bei Nutzung unserer App. <br>
-            Verantwortlicher für die Datenverarbeitung gem. Art. 4 Abs. 7 EU-Datenschutz-Grundverordnung (DSGVO) ist: <br>
-            Valentin Christian Abel<br>
-            Junge Liberale Baden-Württemberg e.V.<br>
-            Rosensteinstraße 22<br>
-            70191 Stuttgart<br>
-            E-Mail: abel@julis-bw.de
-            <div id="p">Der Datenschutzbeauftragte ist</div>
-            Moritz Klammler<br>
-            Junge Liberale Baden-Württemberg e.V.<br>
-            Rosensteinstraße 22<br>
-            70191 Stuttgart<br>
-            E-Mail: datenschutz@julis-bw.de<br>
-            <hr>
-            <h3>GRUNDSÄTZE UNSERER DATENERHEBUNG</h3>
-            Wir erheben in der Regel personenbezogenen Daten im Sinne von Art. 4 Nr. 1 DSGVO: „Alle Informationen, die sich auf eine identifizierte oder identifizierbare natürliche Person (im Folgenden „betroffene Person“) beziehen; als identifizierbar wird eine natürliche Person angesehen, die direkt oder indirekt, insbesondere mittels Zuordnung zu einer Kennung wie einem Namen, zu einer Kennnummer, zu Standortdaten, zu einer Online-Kennung oder zu einem oder mehreren besonderen Merkmalen, die Ausdruck der physischen, physiologischen, genetischen, psychischen, wirtschaftlichen, kulturellen oder sozialen Identität dieser natürlichen Person sind, identifiziert werden kann.“ <br>
-            <div id="p">Solltest du unsere Homepage lediglich informatorisch nutzen, werden die im nächsten Abschnitt genannten Daten aufgrund von Art. 6 Abs. 1 S.1 lit. f DSGVO erhoben.</div>
-            Wenn du mit uns per E-Mail oder über Kontakt-, Mitgliedschafts- oder Serviceformulare Kontakt aufnehmen willst, werden die von dir übersandten Daten (E-Mail-Adresse, ggf. Name, Anschrift, Telefonnummer, Daten zur Kontoverbindung) von uns gespeichert, um die Anfrage zu beantworten. Die in diesem Zusammenhang anfallenden Daten löschen wir, nachdem die Speicherung nicht mehr erforderlich ist oder schränken die Verarbeitung ein, falls gesetzliche Aufbewahrungspflichten bestehen. Bitte beachte, dass die Verwendung von diesen elektronischen Kontaktmöglichkeiten nicht verschlüsselt erfolgt. Sensible Informationen sollten daher immer möglichst nur persönlich weitergegeben oder verschlüsselt versendet werden.
-            <div class="eingerückt">
-                <h4>Bezug unseres Newsletters</h4>
-                Wenn du unseren kostenlosen Newsletter bestellst, werden Vorname, Name und E-Mail-Adresse erhoben. Rechtsgrundlage hierfür ist Art. 6 Abs. 1 lit. a DSGVO. Die Daten werden nur so lange gespeichert, wie es für den Versand notwendig ist. Du kannst jederzeit deine Einwilligung zurückziehen, sodass du keine Newsletter mehr erhältst.Wenn du unseren kostenlosen Newsletter bestellst, werden Vorname, Name und E-Mail-Adresse erhoben. Rechtsgrundlage hierfür ist Art. 6 Abs. 1 lit. a DSGVO. Die Daten werden nur so lange gespeichert, wie es für den Versand notwendig ist. Du kannst jederzeit deine Einwilligung zurückziehen, sodass du keine Newsletter mehr erhältst.
-                <h4>Datenschutz bei den Landesarbeitskreisen</h4>
-                Wenn du dich zu einem oder zu mehreren unserer Landesarbeitskreise anmelden möchtest, erheben wir von dir verschiedene personenbezogene Daten, über welche wir dich hier aufklären.
-                <div id="p">Wir erheben und speichern deinen Vor- und Nachnamen, deine E-Mail-Adresse sowie die Landesarbeitskreise, zu denen du dich anmeldest, um dich über die aktuelle Arbeit deiner Landesarbeitskreise sowie die diese betreffenden Sitzungen und andere Termine zu informieren. Wir erheben außerdem deine Zugehörigkeit zu einem Kreisverband, um überprüfen zu können, ob und wo du Mitglied der Jungen Liberalen Baden-Württemberg bist. Die Information über die Zugehörigkeit zu einem Kreisverband wird nicht dauerhaft gespeichert und nach der Prüfung wieder gelöscht.</div>
-                Deine Daten werden durch den Landesvorstand verwaltet und die Leiter der Landesarbeitskreise haben keinen direkten Zugriff. Die Speicherung deiner Daten erfolgt ausschließlich auf deutschen Servern.
-                <div id="p">Deine Daten werden 30 Tage nach Auflösung deines Landesarbeitskreises am Ende eines jeden Amtsjahres gelöscht. Vor der Löschung erhälst du eine E-Mail, in der wir dich darüber informieren, zu welchen neuen Landesarbeitskreisen du dich anmelden kannst. Solltest du dich von einem Landesarbeitskreis abmelden, löschen wir deine Daten ebenfalls innerhalb von 30 Tagen. Du kannst dich jederzeit von einem Landesarbeitskreis abmelden, sodass du keine E-Mails mehr erhältst.</div>
-            </div>
-            Alle personenbezogenen Daten werden nur so lange gespeichert, wie es für die Zwecke, für die sie verarbeitet werden, erforderlich ist (Grundsatz der „Speicherbegrenzung“ nach Art. 5 Abs. 1 lit. e DSGVO).
-            <div id="p">Sollten wir für Verarbeitung deiner Daten externe Dienstleister beauftragen, werden wir diese sorgfältig auswählen und gesondert beauftragen, sodass dieser im Rahmen einer Auftragsdatenverarbeitung an unsere Weisungen gebunden ist und regelmäßig kontrolliert wird. Wenn dieser Dienstleister seinen Sitz in einem Staat außerhalb des Europäischen Wirtschaftsraums (EWR) hat, informieren wir dich über die Folgen dieses Umstands in der Beschreibung des Angebotes.</div> <hr>
-            
-
-
-        </div>
-        
+    <h2 class="mt-3 text-cyan">DATENSCHUTZERKLÄRUNG</h2>
+    <hr>
+    <h3 class="text-magenta">INFORMATION ÜBER DIE ERHEBUNG PERSONENBEZOGENER DATEN</h3>
+    Im Folgenden informieren wir über Art, Umfang und Zwecke der Erhebung und Verarbeitung personenbezogener Daten bei
+    Nutzung unserer App. <br>
+    Verantwortlicher für die Datenverarbeitung gem. Art. 4 Abs. 7 EU-Datenschutz-Grundverordnung (DSGVO) ist: <br>
+    Valentin Christian Abel<br>
+    Junge Liberale Baden-Württemberg e.V.<br>
+    Rosensteinstraße 22<br>
+    70191 Stuttgart<br>
+    E-Mail: abel@julis-bw.de
+    <p>Der Datenschutzbeauftragte ist</p>
+    Moritz Klammler<br>
+    Junge Liberale Baden-Württemberg e.V.<br>
+    Rosensteinstraße 22<br>
+    70191 Stuttgart<br>
+    E-Mail: datenschutz@julis-bw.de<br>
+    <hr>
+    <h3 class="text-magenta">GRUNDSÄTZE UNSERER DATENERHEBUNG</h3>
+    Wir erheben in der Regel personenbezogenen Daten im Sinne von Art. 4 Nr. 1 DSGVO: „Alle Informationen, die sich
+    auf eine identifizierte oder identifizierbare natürliche Person (im Folgenden „betroffene Person“) beziehen; als
+    identifizierbar wird eine natürliche Person angesehen, die direkt oder indirekt, insbesondere mittels Zuordnung zu
+    einer Kennung wie einem Namen, zu einer Kennnummer, zu Standortdaten, zu einer Online-Kennung oder zu einem oder
+    mehreren besonderen Merkmalen, die Ausdruck der physischen, physiologischen, genetischen, psychischen,
+    wirtschaftlichen, kulturellen oder sozialen Identität dieser natürlichen Person sind, identifiziert werden kann.“
+    <br>
+    <p>Solltest du unsere Homepage lediglich informatorisch nutzen, werden die im nächsten Abschnitt
+      genannten Daten aufgrund von Art. 6 Abs. 1 S.1 lit. f DSGVO erhoben.
+    </p>
+    Wenn du mit uns per E-Mail oder über Kontakt-, Mitgliedschafts- oder Serviceformulare Kontakt aufnehmen willst,
+    werden die von dir übersandten Daten (E-Mail-Adresse, ggf. Name, Anschrift, Telefonnummer, Daten zur
+    Kontoverbindung) von uns gespeichert, um die Anfrage zu beantworten. Die in diesem Zusammenhang anfallenden Daten
+    löschen wir, nachdem die Speicherung nicht mehr erforderlich ist oder schränken die Verarbeitung ein, falls
+    gesetzliche Aufbewahrungspflichten bestehen. Bitte beachte, dass die Verwendung von diesen elektronischen
+    Kontaktmöglichkeiten nicht verschlüsselt erfolgt. Sensible Informationen sollten daher immer möglichst nur
+    persönlich weitergegeben oder verschlüsselt versendet werden.
+    <div class="eingerückt">
+      <h4 class="text-magenta mt-3">Bezug unseres Newsletters</h4>
+      Wenn du unseren kostenlosen Newsletter bestellst, werden Vorname, Name und E-Mail-Adresse erhoben.
+      Rechtsgrundlage hierfür ist Art. 6 Abs. 1 lit. a DSGVO. Die Daten werden nur so lange gespeichert, wie es für
+      den Versand notwendig ist. Du kannst jederzeit deine Einwilligung zurückziehen, sodass du keine Newsletter mehr
+      erhältst.Wenn du unseren kostenlosen Newsletter bestellst, werden Vorname, Name und E-Mail-Adresse erhoben.
+      Rechtsgrundlage hierfür ist Art. 6 Abs. 1 lit. a DSGVO. Die Daten werden nur so lange gespeichert, wie es für
+      den Versand notwendig ist. Du kannst jederzeit deine Einwilligung zurückziehen, sodass du keine Newsletter mehr
+      erhältst.
+      <h4 class="text-magenta mt-3">Datenschutz bei den Landesarbeitskreisen</h4>
+      Wenn du dich zu einem oder zu mehreren unserer Landesarbeitskreise anmelden möchtest, erheben wir von dir
+      verschiedene personenbezogene Daten, über welche wir dich hier aufklären.
+      <div>Wir erheben und speichern deinen Vor- und Nachnamen, deine E-Mail-Adresse sowie die
+        Landesarbeitskreise, zu denen du dich anmeldest, um dich über die aktuelle Arbeit deiner Landesarbeitskreise
+        sowie die diese betreffenden Sitzungen und andere Termine zu informieren. Wir erheben außerdem deine
+        Zugehörigkeit zu einem Kreisverband, um überprüfen zu können, ob und wo du Mitglied der Jungen Liberalen
+        Baden-Württemberg bist. Die Information über die Zugehörigkeit zu einem Kreisverband wird nicht dauerhaft
+        gespeichert und nach der Prüfung wieder gelöscht.
+      </div>
+      Deine Daten werden durch den Landesvorstand verwaltet und die Leiter der Landesarbeitskreise haben keinen
+      direkten Zugriff. Die Speicherung deiner Daten erfolgt ausschließlich auf deutschen Servern.
+      <div>Deine Daten werden 30 Tage nach Auflösung deines Landesarbeitskreises am Ende eines jeden Amtsjahres
+        gelöscht. Vor der Löschung erhälst du eine E-Mail, in der wir dich darüber informieren, zu welchen neuen
+        Landesarbeitskreisen du dich anmelden kannst. Solltest du dich von einem Landesarbeitskreis abmelden, löschen
+        wir deine Daten ebenfalls innerhalb von 30 Tagen. Du kannst dich jederzeit von einem Landesarbeitskreis
+        abmelden, sodass du keine E-Mails mehr erhältst.
+      </div>
     </div>
-    
-</body>    
-</html>
+    Alle personenbezogenen Daten werden nur so lange gespeichert, wie es für die Zwecke, für die sie verarbeitet
+    werden, erforderlich ist (Grundsatz der „Speicherbegrenzung“ nach Art. 5 Abs. 1 lit. e DSGVO).
+    <div>Sollten wir für Verarbeitung deiner Daten externe Dienstleister beauftragen, werden wir diese
+      sorgfältig auswählen und gesondert beauftragen, sodass dieser im Rahmen einer Auftragsdatenverarbeitung an
+      unsere Weisungen gebunden ist und regelmäßig kontrolliert wird. Wenn dieser Dienstleister seinen Sitz in einem
+      Staat außerhalb des Europäischen Wirtschaftsraums (EWR) hat, informieren wir dich über die Folgen dieses
+      Umstands in der Beschreibung des Angebotes.
+    </div>
+    <hr>
+
 
   </div>
 </template>
@@ -72,40 +88,8 @@
 
 <style scoped>
 
-body{
-    color: #404040;
-}
-#p{
-    margin-top: 1%;
-    margin-bottom: 1%;
-}
-h1{
-    text-align: center;
-    color: deeppink;
-    font-weight: bolder;
-}
-h2{
-    text-align: center;
-    color: deepskyblue;
-    font-weight: bold;
-    margin-top: 2%;
-}
-h3{
-    text-align: left;
-    color: deeppink;
-    font-weight: 500;
-    margin-bottom: 1%; 
-    margin-top: 1%;
-}
-h4{
-    text-align: left;
-    color: deeppink;
-    font-weight: 500;
-    margin-bottom: 1%;
-    margin-top: 1%;
-}
-.eingerückt{
+  .eingerückt {
     padding-left: 5%;
-}
+  }
 
 </style>
