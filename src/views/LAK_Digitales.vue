6d54fd0f9a6588f5ae15d7ed5bbfcb598339de8e
sebastianjulis <seb.weiss2003@yahoo.de>
2020-04-10 12:28:47 +0200

    lakdigitales hinzugefügt

---
+++ src/views/LAK_Digitales.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+    <div id="lakbackground">
+      <i class="las la-sitemap"></i>
+      <h1>LAK DIGITALES & MEDIEN</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Verband'
+  }
+</script>
+
+<style scoped>
+
+h1, .la-sitemap{
+  text-align: center; color: white;
+}
+#lakbackground{
+  width: 100%; background-color: $magenta;
+}
+
+</style>
