2646836730ac4f5eef916b8bf7ae1adcdfa7485b
marvw <marvin.wank@outlook.de>
2020-04-10 08:51:39 +0100

    Füge Landingpage für Lak ein

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,58 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '../router'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    landesarbeitskreise: {
+      'Arbeit & Soziales': {
+        'icon': 'suitcase',
+        'href': '/Arbeit und Soziales'
+      },
+      'Bildung & Forschung': {
+        'icon': 'school',
+        'href': '/Bildung und Forschung'
+      },
+      'Digitales & Medien': {
+        'icon': 'server',
+        'href': '/Digitales und Medien'
+      },
+      'Europa & Aussen': {
+        'icon': 'globe',
+        'href': '/Europa und Aussen'
+      },
+      'Finanzen & Raub': {
+        'icon': 'money-bill',
+        'href': '/Finanzen und Steuern'
+      },
+      'Gesellschaft & Kultur': {
+        'icon': 'users',
+        'href': '/Gesellschaft und Kultur'
+      },
+      'Umwelt und Verkehr': {
+        'icon': 'road',
+        'href': '/Umwelt und Verkehr'
+      },
+      'Verbands - entwicklung': {
+        'icon': 'dna',
+        'href': '/Verbandsentwicklung'
+      },
+      'Liberale Schüler': {
+        'icon': 'graduation-cap',
+        'href': '/Verbandsentwicklung'
+      },
+    }
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
+    wechsel_seite (context, neue_seite) {
+      router.push(neue_seite)
+    }
   },
-  modules: {
+  modules: {},
+  getters: {
+    landesarbeitskreise: state => state.landesarbeitskreise
   }
 })
