362264a33ef895d10b13887c2f85ee601039c163
marvw <marvin.wank@outlook.de>
2020-04-10 19:09:29 +0100

    Gebe nun Wörter statt Zahlen bei Monat aus

---
+++ src/content/KalenderHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="kalender-header">
+    <h3 class="text-center">
+      {{ month }} {{ year }}
+    </h3>
+
+    <i class="switch-icon la la-list la-2x"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'KalenderHeader',
+
+    props: [
+      'month',
+      'year'
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Kalender.vue
+++ src/views/Kalender.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
+    <KalenderHeader
+      :month="get_current_month_mapped"
+      :year="current_year"
+    />
+
     <Calendar
       :attributes="attributes"
       is-expanded
       class="kalender"
       @dayclick="change_current_day"
+      @update:to-page="change_month_and_year"
     />
 
     <KalenderVeranstaltungsVorschau
@@ -17,18 +23,22 @@
 
   import Calendar from 'v-calendar/lib/components/calendar.umd'
   import KalenderVeranstaltungsVorschau from '../content/KalenderVeranstaltungsVorschau'
+  import KalenderHeader from '../content/KalenderHeader'
 
   export default {
     name: 'Kalender',
 
     components: {
+      KalenderHeader,
       KalenderVeranstaltungsVorschau,
       Calendar
     },
 
     data () {
       return {
-        'current_day': new Date()
+        'current_day': new Date(),
+        'current_month': new Date().getMonth(),
+        'current_year': new Date().getFullYear()
       }
     },
 
@@ -65,12 +75,33 @@
         }
 
         return termin_tage
+      },
+      get_current_month_mapped(){
+        switch (this.current_month) {
+          case 1: return 'Januar';
+          case 2: return 'Februar';
+          case 3: return 'März';
+          case 4: return 'April';
+          case 5: return 'Mai';
+          case 6: return 'Juni';
+          case 7: return 'Juli';
+          case 8: return 'August';
+          case 9: return 'September';
+          case 10: return 'Oktober';
+          case 11: return 'November';
+          case 12: return 'Dezember';
+          default: return '';
+        }
       }
     },
 
     methods: {
       change_current_day (payload) {
         this.current_day = new Date(payload.date)
+      },
+      change_month_and_year(payload){
+        this.current_month = payload.month;
+        this.current_year = payload.year;
       }
     }
   }
