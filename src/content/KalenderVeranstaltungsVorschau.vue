550ce7a88b2fdc62d82208cd022250b7ec0f2545
marvw <marvin.wank@outlook.de>
2020-04-10 16:39:40 +0100

    Gibt nun Hinweis aus, wenn keine Events stattfinden

--- src/content/KalenderVeranstaltungsVorschau.vue
+++ src/content/KalenderVeranstaltungsVorschau.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+
+    <div v-if="!mindestens_eine_veranstaltung" class="col-12 mb-3">
+      <div class="row veranstaltungs-vorschau-item">
+        <div class="datum-tag keine-veranstaltungen">
+          {{selected_day.getDate()}}
+        </div>
+
+        <div class="info-text keine-veranstaltungen-text">
+          An diesem Tag finden leider keine <br> Veranstaltungen statt
+        </div>
+
+      </div>
+    </div>
+
     <div v-for="(kommende_veranstaltung, index) in anstehende_veranstaltungen"
          v-bind:key="index"
     >
@@ -38,7 +52,8 @@
 
     data(){
       return {
-        'anstehende_veranstaltungen': this.veranstaltungen_liste
+        'anstehende_veranstaltungen': this.veranstaltungen_liste,
+        'mindestens_eine_veranstaltung': false
       }
     },
 
@@ -50,7 +65,6 @@
 
 
     },
-
     methods:{
       gib_anstehende_veranstaltungen () {
         this.anstehende_veranstaltungen = [];
@@ -60,6 +74,9 @@
 
           if (veranstaltung_datum < this.selected_day || veranstaltung.length > 5) {
             continue
+          }
+          if(veranstaltung_datum.getDate() === this.selected_day.getDate()){
+            this.mindestens_eine_veranstaltung = true;
           }
           veranstaltung.Datum = veranstaltung_datum;
           this.anstehende_veranstaltungen.push(veranstaltung)
