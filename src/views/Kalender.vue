6ce96945d8e8f3a8083176d6b4f7d3f243a33b4d
marvw <marvin.wank@outlook.de>
2020-04-10 11:30:36 +0100

    Initialisiere Kalender

--- src/views/Kalender.vue
+++ src/views/Kalender.vue
@@ -1,15 +1,44 @@
 <template>
   <div>
-
+    <Calendar
+      :attributes="attributes"
+      color="red"
+      is-expanded
+      class="kalender"
+    />
   </div>
 </template>
 
 <script>
+
+  import Calendar from 'v-calendar/lib/components/calendar.umd'
   export default {
-    name: 'Kalender'
+    name: 'Kalender',
+
+    components: {
+      Calendar
+    },
+
+
+    computed:{
+      attributes(){
+        return [
+
+          {
+            key: 'today',
+            fillMode: 'light',
+            highlight: 'gray',
+            dates: new Date(),
+          }
+
+        ]
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .kalender{
+    border: 0
+  }
 </style>
