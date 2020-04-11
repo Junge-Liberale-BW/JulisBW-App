af632bd3ec8a86e2c78003f43d435cd31a22acfd
Nils <dev@nilskristjansson.de>
2020-04-11 17:16:00 +0200

    cleanup of helper class

--- src/util/juliette.js
+++ src/util/juliette.js
@@ -1,17 +1,12 @@
-import RSSParser from "rss-parser";
 import axios from "axios"
 
 class JulietteDataService {
-  feedUrl
-  data
-  loading
 
   apiBase = "https://www.julis-bw.de/wp-json/wp/v2"
 
-  constructor(feedUrl) {
+  constructor(apiBase) {
 
-    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
-    this.feedUrl = CORS_PROXY + feedUrl
+    this.apiBase = apiBase
 
   }
 
@@ -85,95 +80,6 @@ class JulietteDataService {
       }
     });
     return Object.keys(unique);
-  }
-
-
-  pollAllData(fn, check, isDone = false, acc) {
-    if (isDone) return
-    const promise = fn(acc)
-    return promise.then(data => this.poll(fn, check, check(data), acc))
-  }
-
-  async fetchData(feedUrl, page, callback, agg = []) {
-    const maxItemsPerPage = 10;
-    let itemsOnLastPage = maxItemsPerPage;
-
-
-    const pagedUrl = `${feedUrl}?paged=${page}`
-    console.log(pagedUrl)
-    try {
-      fetch(pagedUrl).then(data => {
-          if (data.ok) {
-            data.text().then(text => {
-              const parser = new RSSParser();
-              //console.log(text)
-              parser.parseString(text, (err, parsed) => {
-                if (err) {
-                  return (`Error occurred while parsing RSS Feed ${err.toString()}`)
-                } else {
-                  const tItems = parsed.items;
-                  agg.push(...tItems)
-
-                  itemsOnLastPage = tItems.length
-                  if (itemsOnLastPage !== maxItemsPerPage) {
-                    callback(agg);
-                  } else {
-                    return this.fetchData(feedUrl, page + 1, callback, agg)
-                  }
-
-                }
-              });
-            });
-          } else {
-            return ("Error occurred while fetching the feed")
-          }
-        }
-      );
-    } catch (e) {
-      if (e.toString() === "TypeError: Failed to fetch") {
-        return ("Error due to CORS policy")
-      } else {
-        return (e.toString())
-      }
-    }
-  }
-
-  fetchDataP(feedUrl, page, acc) {
-
-    return new Promise((resolve, reject) => {
-
-
-      const pagedUrl = `${feedUrl}?paged=${page}`
-      console.log(pagedUrl)
-      try {
-        fetch(pagedUrl).then(data => {
-            if (data.ok) {
-              data.text().then(text => {
-                const parser = new RSSParser();
-                //console.log(text)
-                parser.parseString(text, (err, parsed) => {
-                  if (err) {
-                    reject(`Error occurred while parsing RSS Feed ${err.toString()}`)
-                  } else {
-                    resolve([...acc, ...parsed.items]);
-
-
-                  }
-                });
-              });
-            } else {
-              reject("Error occurred while fetching the feed")
-            }
-          }
-        );
-      } catch (e) {
-        if (e.toString() === "TypeError: Failed to fetch") {
-          reject("Error due to CORS policy")
-        } else {
-          reject(e.toString())
-        }
-      }
-    });
   }
 
 
