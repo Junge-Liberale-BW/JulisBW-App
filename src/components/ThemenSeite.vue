37409894dc977de67f2f65c039234c5d06b568df
fmundinger <finn@mundinger.de>
2020-04-12 00:19:40 +0200

    added full text for FreiheitSelbstbestimmung

---
+++ src/components/ThemenSeite.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="themenSeite">
+    <div class="container themen-content">
+      <div v-for="item in fixed_content" v-bind:key="item.ueberschrift"
+             class="themen-wrapper"
+        >
+        <p class="themen-title-cyan"> {{item.title}} </p>
+        <p class="themen-p"> {{item.text}} </p>
+      </div>
+        <div v-for="item in foldable_content" v-bind:key="item.ueberschrift"
+             class="themen-wrapper"
+        >
+            <p class="themen-title-magenta"> {{item.title}} </p>
+            <p class="themen-p"> {{item.text}} </p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ThemenSeite',
+    components: {  },
+    props: [
+      'thema',
+      'fixed_content',
+      'foldable_content'
+    ],
+  }
+</script>
+
+<style scoped>
+
+  .lak-seite{
+    font-weight: bold;
+  }
+
+  h1, .la-sitemap {
+    text-align: center;
+    color: white;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .la-sitemap {
+    padding-bottom: 1%;
+    padding-top: 5%;
+  }
+</style>
---
+++ src/views/UnsereThemen.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+
+    <Header/>
+    <div class="kachel-wrapper">
+        <div v-for="(properties, titel) in themen_liste" v-bind:key="titel"
+             class="wide-kachel-wrapper"
+        >
+          <div class="wide-kachel"
+               @click="wechsel_seite('freiheitSelbstbestimmung')"
+               @touchmove="wechsel_seite(titel)"
+          >
+            <i :class=" 'las la-4x la-' + properties.icon + ' wide-kachel-i'"></i>
+            <br>
+            {{titel}}
+          </div>
+        </div>
+    </div>
+  </div>
+
+</template>
+<script>
+  import Header from '../components/Header'
+
+  export default {
+    name: 'UnsereThemen',
+    components: { Header },
+    computed: {
+      themen_liste () {
+        return this.$store.getters.unsereThemen
+      }
+    },
+
+    methods: {
+      wechsel_seite (titel) {
+        this.$store.dispatch('wechsel_seite', titel)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
