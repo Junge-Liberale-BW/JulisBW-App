025dc6f1471f736a52f8c7051406511766a7d076
Lootcifer <elvi.karl@web.de>
2020-04-12 23:46:21 +0200

    Kommentare hinzugefügt und Code aufgeräumt

--- src/components/rss/Article.vue
+++ src/components/rss/Article.vue
@@ -13,29 +13,24 @@
 <script>
 
   //import Media from "cordova-plugin-media";
-
-  const parseDate = tdate => {
-    const systemDate = new Date(Date.parse(tdate));
-    const userDate = new Date();
-    const diff = Math.floor((userDate - systemDate) / 1000);
-    //let myMedia = new Media(this.article.link);
-    //myMedia.play();
+  //let myMedia = new Media(this.article.link);
+  //myMedia.play();
 
 
-    if (diff < 59) {
-      return diff + "s";
-    }
-    if (diff <= 3540) {
-      return Math.round(diff / 60) + "m";
-    }
-    if (diff <= 86400) {
-      return Math.round(diff / 3600) + "h";
-    }
-    if (diff < 604800) {
-      return Math.round(diff / 86400) + "d";
-    }
-    return systemDate.toString().substring(4, 10);
-  };
+  /*
+  TODO: Css, MP3Player aufruf
+
+  Übergabe an den musikplayer:
+      article.link //link der mp3
+      article.content //Podcast-Beschreibung
+      article.title //Folge: Titel, Author
+      article.isoDate //das Format sollte noch geändert werden
+
+      bzw this.article je nachdem wo man es benutzt
+
+
+   */
+
 
   export default {
     name: "Article",
@@ -48,11 +43,6 @@
     selected: false,
     methods: {
 
-      getDateTime() {
-        if (this.article.isoDate) {
-          return parseDate(this.article.isoDate);
-        }
-      }
     }
   };
 
